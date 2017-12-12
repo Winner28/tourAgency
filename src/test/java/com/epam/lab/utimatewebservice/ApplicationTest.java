8be09e69b65e952b94a769ea61fa9acaba3644a2
Andrei Elagin <andrei_elagin@epam.com>
2017-12-13 00:56:45 +0300

    TASK-0: - initial commit - basic project structure created

---
+++ src/test/java/com/epam/lab/utimatewebservice/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.epam.lab.utimatewebservice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApplicationTest {
+
+    @Test
+    public void mainTest() {
+    }
+}
