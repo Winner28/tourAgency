4d3a0c714b16467123b13ca11c5e93d9d318e3e3
Vlad <bagdeyv@gmail.com>
2017-12-15 17:37:16 +0300

    Task-14 main и test добавлены

---
+++ src/main/java/com/epam/lab/ultimatewebservice/config/SpringRootConfig.java
@@ -0,0 +1,9 @@
+package com.epam.lab.ultimatewebservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({ "com.epam.lab.ultimatewebservice.service" })
+public class SpringRootConfig {
+}
