18fc432c6c7d713b829d2c6073c2d2bf2979d7ca
Vlad <bagdeyv@gmail.com>
2017-12-17 22:10:13 +0300

    upd;

--- src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
+++ src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
@@ -1,43 +1,8 @@
 package com.epam.lab.ultimatewebservice.db.dao;
 
-import com.epam.lab.ultimatewebservice.db.connpool.ConnectionPool;
-import com.epam.lab.ultimatewebservice.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Optional;
-
 
 public class UserDAO {
 
-    private JdbcDAO jdbcDAO;
-
-    @Autowired
-    public UserDAO(ConnectionPool connectionPool) {
-        jdbcDAO = connectionPool::getConnection;
-    }
-
-    public Optional<User> getUserById(int id) {
-        return Optional.ofNullable(jdbcDAO.mapPreparedStatement(preparedStatement -> {
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return new User()
-                            .setId(id)
-                            .setFirstname(resultSet.getString("firstname"))
-                            .setLastname(resultSet.getString("lastname"))
-                            .setEmail(resultSet.getString("email"))
-                            .setPassword_hash(resultSet.getString("password_hash"));
-                } else {
-                    return null;
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return null;
-            }
-
-        }, "SELECT id, firstname, lastname, email, password_hash FROM Users WHERE id=?", id));
-    }
 
 }
 
