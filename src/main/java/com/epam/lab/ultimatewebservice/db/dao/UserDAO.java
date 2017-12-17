97a7d066f628c544f11826a55fb06e8fb9389315
Vlad <bagdeyv@gmail.com>
2017-12-17 23:02:13 +0300

    TASK-18 UserDAO upd;

--- src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
+++ src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
@@ -1,28 +1,43 @@
 package com.epam.lab.ultimatewebservice.db.dao;
 
-public class UserDAO {
-    private static UserDAO instance;
+import com.epam.lab.ultimatewebservice.db.connpool.ConnectionPool;
+import com.epam.lab.ultimatewebservice.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
-    private static final String ADD_USER_SCRIPT =
+public class UserDAO {
+
+    private final String ADD_USER_SCRIPT =
             "INSERT INTO users(first_name, last_name, email, password_hash) VALUES(?,?,?,?)";
-    private static final String DELETE_USER_BYID =
+    private final String DELETE_USER_BYID =
             "DELETE FROM users WHERE id=?";
-    private static final String GET_ALL_USERS =
+    private final String GET_ALL_USERS =
             "SELECT (first_name, last_name, email, password_hash) FROM users";
-    private static final String GET_USER_BYID =
+    private final String GET_USER_BYID =
             "SELECT (first_name, last_name, email, password_hash) FROM users WHERE id=?";
-    private static final String GET_USER_BY_EMAIL =
+    private final String GET_USER_BY_EMAIL =
             "SELECT (first_name, last_name, email, password_hash) FROM users WHERE email=?";
-    private static final String UPDATE_USER =
+    private final String UPDATE_USER =
             "UPDATE users SET first_name=?, last_name=?, email=?, password_hash=? WHERE id=?";
 
-    private UserDAO() {}
+    private JdbcDAO jdbcDAO;
 
-    public static synchronized UserDAO getInstance() {
-        if (instance == null)
-            instance = new UserDAO();
-        return instance;
+    @Autowired
+    public UserDAO(ConnectionPool connectionPool) {
+        jdbcDAO = () -> {
+            try {
+                return connectionPool.getConnection();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        };
     }
+
+
 
     public void addUser(){
 
@@ -37,15 +52,34 @@ public class UserDAO {
     }
 
     public List<User> getAllUsers() {
-
+        return null;
     }
 
-    public Optional<User> getUserByID(int id){
+    public Optional<User> getUserById(int id) {
+        return Optional.ofNullable(jdbcDAO.mapPreparedStatement(preparedStatement -> {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return new User()
+                            .setId(id)
+                            .setFirstname(resultSet.getString("firstname"))
+                            .setLastname(resultSet.getString("lastname"))
+                            .setEmail(resultSet.getString("email"))
+                            .setPassword_hash(resultSet.getString("password_hash"));
+                } else {
+                    return null;
+                }
 
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }, "SELECT id, firstname, lastname, email, password_hash FROM Users WHERE id=?", id));
     }
+
 
     public Optional<User> getUserByEmail(String email){
-
+        return null;
     }
 
 }
