66f165d67d022ac0c138317cd4c3ed8e2eb74c69
wladez <wladez95@gmail.com>
2017-12-17 21:31:43 +0300

    Added constants and methods without implementation for UserDAO

--- src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
+++ src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
@@ -1,4 +1,51 @@
 package com.epam.lab.ultimatewebservice.db.dao;
 
 public class UserDAO {
+    private static UserDAO instance;
+
+    private static final String ADD_USER_SCRIPT =
+            "INSERT INTO users(first_name, last_name, email, password_hash) VALUES(?,?,?,?)";
+    private static final String DELETE_USER_BYID =
+            "DELETE FROM users WHERE id=?";
+    private static final String GET_ALL_USERS =
+            "SELECT (first_name, last_name, email, password_hash) FROM users";
+    private static final String GET_USER_BYID =
+            "SELECT (first_name, last_name, email, password_hash) FROM users WHERE id=?";
+    private static final String GET_USER_BY_EMAIL =
+            "SELECT (first_name, last_name, email, password_hash) FROM users WHERE email=?";
+    private static final String UPDATE_USER =
+            "UPDATE users SET first_name=?, last_name=?, email=?, password_hash=? WHERE id=?";
+
+    private UserDAO() {}
+
+    public static synchronized UserDAO getInstance() {
+        if (instance == null)
+            instance = new UserDAO();
+        return instance;
+    }
+
+    public void addUser(){
+
+    }
+
+    public void updateUser(){
+
+    }
+
+    public void deleteUserById(){
+
+    }
+
+    public List<User> getAllUsers() {
+
+    }
+
+    public Optional<User> getUserByID(int id){
+
+    }
+
+    public Optional<User> getUserByEmail(String email){
+
+    }
+
 }
