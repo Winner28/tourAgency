cc64b58401b4423c68fd323b1255beb0ac55e029
Vlad <bagdeyv@gmail.com>
2017-12-18 12:17:01 +0300

    Added implementation of getAllUsers method

--- src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
+++ src/main/java/com/epam/lab/ultimatewebservice/db/dao/UserDAO.java
@@ -5,18 +5,19 @@ import com.epam.lab.ultimatewebservice.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class UserDAO {
 
-    private final String ADD_USER_SCRIPT =
+    private final String ADD_USER =
             "INSERT INTO users(first_name, last_name, email, password_hash) VALUES(?,?,?,?)";
-    private final String DELETE_USER_BYID =
+    private final String DELETE_USER_BY_ID =
             "DELETE FROM users WHERE id=?";
     private final String GET_ALL_USERS =
             "SELECT (first_name, last_name, email, password_hash) FROM users";
-    private final String GET_USER_BYID =
+    private final String GET_USER_BY_ID =
             "SELECT (first_name, last_name, email, password_hash) FROM users WHERE id=?";
     private final String GET_USER_BY_EMAIL =
             "SELECT (first_name, last_name, email, password_hash) FROM users WHERE email=?";
@@ -25,22 +26,21 @@ public class UserDAO {
 
     private JdbcDAO jdbcDAO;
 
+
     @Autowired
     public UserDAO(ConnectionPool connectionPool) {
         jdbcDAO = () -> {
             try {
                 return connectionPool.getConnection();
             } catch (InterruptedException e) {
-                e.printStackTrace();
-                return null;
+                throw new RuntimeException("Exception with ConnectionPool getConnection");
             }
         };
     }
 
 
 
-    public void addUser(){
-
+    public void addUser() {
     }
 
     public void updateUser(){
@@ -52,7 +52,24 @@ public class UserDAO {
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> userList = new ArrayList<>();
+        jdbcDAO.withResultSet(rs -> {
+            try {
+                while ((rs.next())) {
+                    userList.add(new User()
+                            .setId(rs.getInt("id"))
+                            .setFirstname(rs.getString("firstname"))
+                            .setLastname(rs.getString("lastname"))
+                            .setEmail(rs.getString("email"))
+                            .setPassword_hash(rs.getString("password_hash"))
+
+                    );
+                }
+            }catch (SQLException e) {
+                throw new RuntimeException("method getAllUsers throws exception");
+            }
+        }, GET_ALL_USERS);
+        return userList;
     }
 
     public Optional<User> getUserById(int id) {
@@ -74,7 +91,7 @@ public class UserDAO {
                 return null;
             }
 
-        }, "SELECT id, firstname, lastname, email, password_hash FROM Users WHERE id=?", id));
+        }, GET_USER_BY_ID, id));
     }
 
 
