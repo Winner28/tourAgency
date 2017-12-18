280d83f434873c35039fdb39012bdd38abddf157
vladey <bagdeyv@gmail.com>
2017-12-19 01:44:46 +0300

    added class UserNotFoundException; skeleton of UserController added;

--- src/main/java/com/epam/lab/ultimatewebservice/controllers/UserController.java
+++ src/main/java/com/epam/lab/ultimatewebservice/controllers/UserController.java
@@ -1,15 +1,16 @@
 package com.epam.lab.ultimatewebservice.controllers;
 
 import com.epam.lab.ultimatewebservice.entity.User;
+import com.epam.lab.ultimatewebservice.exceptions.UserNotFoundException;
 import com.epam.lab.ultimatewebservice.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.soap.SOAPBinding;
 
 @Controller
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -17,18 +18,28 @@ public class UserController {
 
     private final UserService userService;
     
-    @RequestMapping(value = "/users/get/{user_id}", method = RequestMethod.GET)
-    public ModelAndView getUserById(@PathVariable(value = "user_id") int user_id) {
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public ModelAndView getUserById(@PathVariable(value = "id") int id) {
         ModelAndView model = new ModelAndView();
-        User user = userService.getUserById(user_id);
-        model.setViewName("user");
-        model.addObject("user", user);
+   /*     User user = userService.getUserById(user_id);
+        model.addObject("user", user);*/
         return model;
     }
 
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public ModelAndView createUser( @RequestBody String user) {
+        return null;
+    }
 
-    private boolean validateUser(User user) {
-        return true;
+
+    @RequestMapping(value = "/users/{id}")
+    public ModelAndView deleteUser() {
+        return null;
+    }
+
+    private void validateUser(String id) {
+        if(false)
+            throw new UserNotFoundException("exception");
     }
 
 }
