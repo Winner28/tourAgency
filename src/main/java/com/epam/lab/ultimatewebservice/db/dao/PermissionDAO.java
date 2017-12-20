e5d53e7f657298241cfacd8f6afae812b34e4f28
Vlad <bagdeyv@gmail.com>
2017-12-20 17:10:40 +0300

    TASK-21: "Create PermissionDAO" added constructor and final static fields

--- src/main/java/com/epam/lab/ultimatewebservice/db/dao/PermissionDAO.java
+++ src/main/java/com/epam/lab/ultimatewebservice/db/dao/PermissionDAO.java
@@ -1,4 +1,36 @@
 package com.epam.lab.ultimatewebservice.db.dao;
 
+import com.epam.lab.ultimatewebservice.db.connpool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PermissionDAO {
+
+    private final static String ADD_USER_PERMISSION =
+            "INSERT INTO Permissions(userId, permissionNameId) VALUES(?,?)";
+    private final static String DELETE_USER_PERMISSION =
+            "DELETE FROM Permissions WHERE userId=?";
+/*    private final static String GET_ALL_USERS_ID_BY_PERMISSION_NAME =
+            "SELECT (firstname, lastname, email, password_hash) FROM Users";
+    private final static String GET_ALL_USERS =
+            "SELECT (firstname, lastname, email, password_hash) FROM Users";*/
+
+    private final static String USER_ID = "userId";
+    private final static String PERMISSION_NAME_ID= "permissionNameId";
+
+
+    private JdbcDAO jdbcDAO;
+
+    @Autowired
+    public PermissionDAO(ConnectionPool connectionPool) {
+        jdbcDAO = () -> {
+            try {
+                return connectionPool.getConnection();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        };
+    }
 }
