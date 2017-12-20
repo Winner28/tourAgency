05f8b9a6b8c00d20ddc834040a76ae760e98ab53
Vlad <bagdeyv@gmail.com>
2017-12-20 19:04:34 +0300

    TASK-22: "Create TourTypeDAO" added constructor and static fields;

--- src/main/java/com/epam/lab/ultimatewebservice/db/dao/TourTypeDAO.java
+++ src/main/java/com/epam/lab/ultimatewebservice/db/dao/TourTypeDAO.java
@@ -1,4 +1,36 @@
 package com.epam.lab.ultimatewebservice.db.dao;
 
+import com.epam.lab.ultimatewebservice.db.connpool.ConnectionPool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TourTypeDAO {
+
+    private final static String ADD_TOUR =
+            "INSERT INTO Tour_Types(id, tour_type) VALUES(?,?)";
+    private final static String DELETE_TOUR =
+            "DELETE FROM Tour_Types WHERE id=?";
+    private final static String GET_ALL_TOURS =
+            "SELECT id, tour_type FROM Tour_Types";
+    private final static String GET_TOUR_BY_ID =
+            "SELECT id, tour_type FROM Tour_Types WHERE id=?";
+    private final static String UPDATE_TOUR_BY_ID =
+            "UPDATE Tour_Types SET tour_type=? WHERE id=?";
+    private final static String ID = "id";
+    private final static String TOUR_TYPE= "tour_type";
+
+    private JdbcDAO jdbcDAO;
+
+    @Autowired
+    public TourTypeDAO(ConnectionPool connectionPool) {
+        jdbcDAO = () -> {
+            try {
+                return connectionPool.getConnection();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        };
+    }
 }
