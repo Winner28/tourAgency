2e57f511f5d5b6fa8418d86b3e86348e716875b7
Vlad <bagdeyv@gmail.com>
2017-12-20 16:52:51 +0300

    renamed some entity

---
+++ src/main/java/com/epam/lab/ultimatewebservice/entity/Permission.java
@@ -0,0 +1,1 @@
+package com.epam.lab.ultimatewebservice.entity;import lombok.Data;import lombok.experimental.Accessors;@Data@Accessors(chain = true)public class Permission {    private int userId;    private int permissionNameId;}
