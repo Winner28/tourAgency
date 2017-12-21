433e1caef1b43a0ebe1b7e35bbbf0e33ebad1a8a
Vlad <bagdeyv@gmail.com>
2017-12-21 18:41:44 +0300

    sessionManager methods added;

--- src/main/java/com/epam/lab/ultimatewebservice/controllers/SessionManager.java
+++ src/main/java/com/epam/lab/ultimatewebservice/controllers/SessionManager.java
@@ -4,24 +4,33 @@ package com.epam.lab.ultimatewebservice.controllers;
 import com.epam.lab.ultimatewebservice.entity.User;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class SessionManager {
 
-    /*private static Map<Integer, String> sessionMap = new HashMap<>();
+    private static Map<String, String> sessionMap = new ConcurrentHashMap<>();
+    private static String cookieName = "userLoggedIn";
 
-    public static void createNewSession(User user, HttpServletRequest request) {
-        HttpSession session = request.getSession(true);
-        String sessionId = session.getId();
-        sessionMap.put(user.getId(), sessionId);
+    public static void createNewSession(String id, Cookie cookie) {
+        if (cookie.getName().equals(cookieName)) {
+            sessionMap.put(cookie.getValue(), id);
+        }
     }
 
-    public static boolean checkIfUserLogined(User user) {
-        return sessionMap.get(user.getId()) != null;
+    public static String checkIfUserLogined(Cookie cookie) {
+        if (cookie.getName().equals(cookieName)) {
+            if (sessionMap.get(cookie.getValue()) != null) {
+                return sessionMap.get(cookie.getValue());
+            } else {
+                return null;
+            }
+        }
+        return null;
     }
-    */
 }
