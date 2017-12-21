38dc2c350aeabaebde7825052f76d06a3c5c9158
Vlad <bagdeyv@gmail.com>
2017-12-21 18:51:04 +0300

    SessionManager is ready to merge

--- src/main/java/com/epam/lab/ultimatewebservice/controllers/SessionManager.java
+++ src/main/java/com/epam/lab/ultimatewebservice/controllers/SessionManager.java
@@ -1,35 +1,27 @@
 package com.epam.lab.ultimatewebservice.controllers;
 
 
-import com.epam.lab.ultimatewebservice.entity.User;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class SessionManager {
 
-    private static Map<String, String> sessionMap = new ConcurrentHashMap<>();
-    private static String cookieName = "userLoggedIn";
+    private static final Map<String, String> SESSIONS = new ConcurrentHashMap<>();
+    private static final String COOKIE_NAME = "userLoggedIn";
 
-    public static void createNewSession(String id, Cookie cookie) {
-        if (cookie.getName().equals(cookieName)) {
-            sessionMap.put(cookie.getValue(), id);
+    public static void createNewSession(Cookie cookie, String id) {
+        if (cookie.getName().equals(COOKIE_NAME)) {
+            SESSIONS.put(cookie.getValue(), id);
         }
     }
 
     public static String checkIfUserLogined(Cookie cookie) {
-        if (cookie.getName().equals(cookieName)) {
-            if (sessionMap.get(cookie.getValue()) != null) {
-                return sessionMap.get(cookie.getValue());
-            } else {
-                return null;
-            }
+        if (cookie.getName().equals(COOKIE_NAME) && SESSIONS.get(cookie.getValue()) != null) {
+                return SESSIONS.get(cookie.getValue());
         }
         return null;
     }
